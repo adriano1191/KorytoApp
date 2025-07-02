93f734ac0d67351883a86fb754489de6eddc725d
Adrian <adino1191@gmail.com>
2025-07-02 18:56:36 +0200

    Update Edit user and improved user save

--- KorytoApp/ViewModels/UserViewModel.cs
+++ KorytoApp/ViewModels/UserViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using KorytoApp.Models;
 using KorytoApp.Services;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -16,21 +17,25 @@ namespace KorytoApp.ViewModels
             _userService = new UserService(Data.AppDatabase.GetConnection());
             LoadUserCommand.Execute(null);
         }
+        public ObservableCollection<string> Genders { get; } = new() { "Mężczyzna", "Kobieta" };
 
+        [ObservableProperty] private int userId;
         [ObservableProperty] private string name;
-        [ObservableProperty] private string gender;
+        //[ObservableProperty] private string gender;
         [ObservableProperty] private int height;
         [ObservableProperty] private int weight;
-
+        [ObservableProperty] private string gender;
         [ObservableProperty] private bool isEdit = false;
         [ObservableProperty] public bool isReadOnly = true;
+
+      
 
         [RelayCommand]
         private async Task SaveUser()
         {
             if (string.IsNullOrWhiteSpace(Name) || Height <= 0 || Weight <= 0)
                 return;
-
+            var existingUser = await _userService.GetUserAsync();
             var user = new User
             {
                 UserName = Name,
@@ -39,9 +44,19 @@ namespace KorytoApp.ViewModels
                 UserWeight = Weight
             };
 
-            await _userService.AddUser(user);
+            if (existingUser != null)
+            {
+                user.Id = existingUser.Id; // ważne: przypisz ID istniejącego użytkownika
+                await _userService.UpdateUser(user);
+            }
+            else
+            {
+                await _userService.AddUser(user);
+            }
             ToggleEdit();
         }
+
+
 
         [RelayCommand]
         private async Task LoadUser()
@@ -49,6 +64,7 @@ namespace KorytoApp.ViewModels
             var user = await _userService.GetUserAsync();
             if (user != null)
             {
+                UserId = user.Id;
                 Name = user.UserName;
                 Gender = user.UserGender;
                 Height = user.UserHeight;
