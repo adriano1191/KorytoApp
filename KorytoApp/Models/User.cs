9f84f122d16b8d7ca8dce4a126ba3b33c7e34000
Adrian <adino1191@gmail.com>
2025-07-02 21:29:04 +0200

    User Data Add: BMI BMR Water TDEE
    Fix display userdata

--- KorytoApp/Models/User.cs
+++ KorytoApp/Models/User.cs
@@ -8,7 +8,11 @@ namespace KorytoApp.Models
 
         public string UserName { get; set; }
         public string UserGender { get; set; } // lub enum
+        public int UserAge { get; set; } // lata
         public int UserHeight { get; set; } // cm
         public int UserWeight { get; set; } // kg
+
+        public double UserActivity { get; set; } // lub enum
+
     }
 }
--- KorytoApp/ViewModels/UserViewModel.cs
+++ KorytoApp/ViewModels/UserViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using KorytoApp.Models;
 using KorytoApp.Services;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -17,18 +18,65 @@ namespace KorytoApp.ViewModels
             _userService = new UserService(Data.AppDatabase.GetConnection());
             LoadUserCommand.Execute(null);
         }
-        public ObservableCollection<string> Genders { get; } = new() { "Mężczyzna", "Kobieta" };
+        public ObservableCollection<GenderOption> GenderOptions { get; } = new()
+{
+        new GenderOption { GenderLabel = "Mężczyzna", GenderValue = "M" },
+        new GenderOption { GenderLabel = "Kobieta", GenderValue = "F" }
+        };
 
-        [ObservableProperty] private int userId;
-        [ObservableProperty] private string name;
+        public ObservableCollection<ActivityOption> ActivityOptions { get; } = new()
+{
+        new ActivityOption { ActivityLabel = "Brak ruchu", ActivityValue = 1.2 },
+        new ActivityOption { ActivityLabel = "Lekka aktywność", ActivityValue = 1.375 },
+        new ActivityOption { ActivityLabel = "Średnia aktywność", ActivityValue = 1.55 },
+        new ActivityOption { ActivityLabel = "Duża aktywność", ActivityValue = 1.725 },
+        new ActivityOption { ActivityLabel = "Bardzo duża aktywność", ActivityValue = 1.9 }
+        };
+
+        [ObservableProperty] private int userId = 0;
+        [ObservableProperty] private string name = "name";
+        [ObservableProperty] private int age = 0;
+        [ObservableProperty] private ActivityOption selectedActivity = new ActivityOption
+        {
+            ActivityLabel = "Brak ruchu",
+            ActivityValue = 1.2
+        };
         //[ObservableProperty] private string gender;
-        [ObservableProperty] private int height;
-        [ObservableProperty] private int weight;
-        [ObservableProperty] private string gender;
+        [ObservableProperty] private int height = 0;
+        [ObservableProperty] private int weight = 0;
+        [ObservableProperty] private GenderOption selectedGender = new GenderOption
+        {
+            GenderLabel = "Mężczyzna",
+            GenderValue = "M"
+        };
         [ObservableProperty] private bool isEdit = false;
         [ObservableProperty] public bool isReadOnly = true;
 
-      
+        [ObservableProperty] public double bmi = 0;
+        [ObservableProperty] public double bmr = 0;
+        [ObservableProperty] public double water = 0;
+        [ObservableProperty] public double tdee = 0;
+
+        public string BmiText => $"BMI: {Bmi:F1}";
+        public string BmrText => $"BMR: {Bmr:F0} kcal";
+        public string WaterText => $"Woda: {Water:F2}l";
+        public string TdeeText => $"TDEE (dzienne zapotrzebowanie): {Tdee:F0} kcal";
+
+        public class GenderOption
+        {
+            public string GenderLabel { get; set; }     // widoczne w Pickerze
+            public string GenderValue { get; set; }     // zapisywane do bazy
+
+            public override string ToString() => GenderLabel; // żeby Picker wyświetlał Label
+        }
+
+        public class ActivityOption
+        {
+            public string ActivityLabel { get; set; }     // widoczne w Pickerze
+            public double ActivityValue { get; set; }     // zapisywane do bazy
+
+            public override string ToString() => ActivityLabel; // żeby Picker wyświetlał Label
+        }
 
         [RelayCommand]
         private async Task SaveUser()
@@ -39,9 +87,12 @@ namespace KorytoApp.ViewModels
             var user = new User
             {
                 UserName = Name,
-                UserGender = Gender,
+                UserAge = Age,
+                UserGender = SelectedGender?.GenderValue,
                 UserHeight = Height,
-                UserWeight = Weight
+                UserWeight = Weight,
+                UserActivity = (double)(SelectedActivity?.ActivityValue)
+
             };
 
             if (existingUser != null)
@@ -54,6 +105,10 @@ namespace KorytoApp.ViewModels
                 await _userService.AddUser(user);
             }
             ToggleEdit();
+            BMICalculate(Weight, Height);
+            BMRCalculate(Weight, Height, Age);
+            WaterCalculate(Weight);
+            TDEECalculate(Bmr);
         }
 
 
@@ -66,10 +121,18 @@ namespace KorytoApp.ViewModels
             {
                 UserId = user.Id;
                 Name = user.UserName;
-                Gender = user.UserGender;
+                Age = user.UserAge;
+                SelectedGender = GenderOptions.FirstOrDefault(g => g.GenderValue == user.UserGender);
                 Height = user.UserHeight;
                 Weight = user.UserWeight;
+                SelectedActivity = ActivityOptions.FirstOrDefault(h => h.ActivityValue == user.UserActivity);
             }
+
+
+            BMICalculate(Weight, Height);
+            BMRCalculate(Weight, Height, Age);
+            WaterCalculate(Weight);
+            TDEECalculate(Bmr);
         }
         [RelayCommand]
         private void ToggleEdit()
@@ -77,6 +140,53 @@ namespace KorytoApp.ViewModels
             IsEdit = !IsEdit;
             IsReadOnly = !IsReadOnly;
         }
+
+        private void BMICalculate(int weight, int height)
+        {
+            double heightInMeters = height / 100.0;
+            Bmi = weight / (heightInMeters * heightInMeters);
+            OnPropertyChanged(nameof(BmiText));
+        }
+
+        private void BMRCalculate(int weight, int height, int age)
+        {
+            if (SelectedGender?.GenderValue == "M")
+            {
+                Bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+            }
+            else
+            {
+                Bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+            }
+            
+            OnPropertyChanged(nameof(BmrText));
+        }
+
+        private void WaterCalculate(int weight)
+        {
+            double totalWater = weight * 0.033;
+            double plainWater = totalWater * 0.7;
+
+            // Dodatkowy limit bezpieczeństwa
+            Water = Math.Min(plainWater, 3.5); // nie więcej niż 3 litry
+            OnPropertyChanged(nameof(WaterText));
+
+        }
+
+        private void TDEECalculate(double bmr)
+        {
+            if (SelectedActivity != null)
+            {
+                Tdee = (double)(bmr * SelectedActivity.ActivityValue);
+            }
+            else
+            {
+                Tdee = (double)(bmr * 1.2);
+            }
+            OnPropertyChanged(nameof(TdeeText));
+
+        }
+
 
     }
 }
